ef27e6bc067d6839cac1c617be34e46368cd5668
RayaneKaddoury <rayanekaddoury@gmail.com>
2022-06-21 13:30:20 +0200

    reset

--- server/function_token/jwtTokens.mjs
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export default function jwtTokens({id,name,email}){
-    const user = {id,name,email};
-    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:"24h"});
-    const refreshToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:"3600s"});
-    return ({accessToken,refreshToken})   
-}
